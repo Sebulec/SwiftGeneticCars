3267ba15f035bf3ba71a082d1c8f8427bc6323de
Sebastian Kotarski <sebastian.kotarski@yumasoft.pl>
2017-10-25 20:00:59 +0200

    Initial commit.

---
+++ GeneticCars/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GeneticCars
+//
+//  Created by Sebastian Kotarski on 25.10.2017.
+//  Copyright © 2017 Sebastian Kotarski. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
